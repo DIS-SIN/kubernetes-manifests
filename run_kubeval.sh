e9cd9331660848bab6f3ce0cf8c6938290f61142
Steven Zinck <szinck@gmail.com>
2019-05-29 09:16:59 -0300

    fix

--- run_kubeval.sh
+++ run_kubeval.sh
@@ -1,6 +1,6 @@
 for filename in `find . ! -name '.travis.yml' -type f  \( -iname \*.yaml -o -iname \*.yml \)` ; do
   [ -f "${filename}" ] || break
-  kubeval "${filename}"
+  ./kubeval "${filename}"
   if [ $? != 0 ] ; then
     echo "${filename}: Failed." && break
   fi
