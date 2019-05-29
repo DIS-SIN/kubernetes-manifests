1dc4f66bf9147adc8842aa9e0d50b2490889d1d7
Steven Zinck <szinck@gmail.com>
2019-05-29 09:12:42 -0300

    add travis config

--- run_kubeval.sh
+++ run_kubeval.sh
@@ -1,5 +1,9 @@
-for filename in `find ./ -type f \( -iname \*.yaml -o -iname \*.yml \)` ; do 
+for filename in `find . ! -name '.travis.yml' -type f  \( -iname \*.yaml -o -iname \*.yml \)`
   [ -f "${filename}" ] || break
   ./kubeval "${filename}"
+  if [ $? != 0 ] ; then
+    echo 
+    echo "${filename}: Failed." && break
+  fi
 done
 
