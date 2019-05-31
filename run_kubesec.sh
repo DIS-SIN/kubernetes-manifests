71ee18c6f3758ece8b7a66929392dff8325d61c9
Steven Zinck <szinck@gmail.com>
2019-05-31 10:45:35 -0300

    use find.. doh

--- run_kubesec.sh
+++ run_kubesec.sh
@@ -2,7 +2,7 @@
 
 score=0
 
-for filename in *yaml *yml ; do
+for filename in `find . ! -name '.travis.yml' -type f  \( -iname \*.yaml -o -iname \*.yml \)` ; do
   [ -f "${filename}" ] || break
   docker run -i kubesec/kubesec:d432be9 scan /dev/stdin < ${filename} | jq --exit-status ".[].score == ${score}" && echo ${filename} has a score higher than ${score} && break
 
