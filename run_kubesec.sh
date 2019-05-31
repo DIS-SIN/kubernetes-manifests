f72ee76aeefaba043fd1f49842e2d3304131e353
Steven Zinck <szinck@gmail.com>
2019-05-31 10:50:51 -0300

    fail on score >0

--- run_kubesec.sh
+++ run_kubesec.sh
@@ -4,7 +4,7 @@ score=0
 
 for filename in `find . ! -name '.travis.yml' -type f  \( -iname \*.yaml -o -iname \*.yml \)` ; do
   [ -f "${filename}" ] || break
-  docker run -i kubesec/kubesec:d432be9 scan /dev/stdin < ${filename} | jq --exit-status ".[].score == ${score}" && echo ${filename} has a score higher than ${score} && break
+  docker run -i kubesec/kubesec:d432be9 scan /dev/stdin < ${filename} | jq --exit-status ".[].score > ${score}" && echo ${filename} has a score higher than ${score} && break
 
 done
 
